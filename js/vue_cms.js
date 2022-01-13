b85f19a19b9dc93ac98ae27372d37dbe7e2a1e14
Jan <55839828+Piitschy@users.noreply.github.com>
2022-01-13 19:32:35 +0100

    switch to vue-petit

--- js/vue_cms.js
+++ js/vue_cms.js
@@ -1,13 +1,18 @@
-var cms = new Vue({
-  el: '#cms',
-  data() {
-    return {
-      items: []
-    }
+import { createApp } from 'https://unpkg.com/petite-vue?module'
+
+createApp({
+  // data
+  url:'https://cms.spiriert.de/',
+  data: {
+    items: []
+  },
+  // methods
+  load(route,target) {
+    fetch(this.url+route)
+      .then(response => response.json())
+      .then(data => this.data[target] = data)
   },
   mounted() {
-    fetch('https://cms.spiriert.de/texts')
-      .then(response => response.json())
-      .then(data => this.items = data)
+    this.load('texts','items')
   }
-})
+}).mount()
