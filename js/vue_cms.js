a89f51b568a4e8fb4656920f56a8060f10a2bb74
Jan <55839828+Piitschy@users.noreply.github.com>
2022-01-15 19:46:53 +0100

    prepare linkboxes for cms

--- js/vue_cms.js
+++ js/vue_cms.js
@@ -3,17 +3,24 @@ import { createApp } from 'https://unpkg.com/petite-vue?module'
 createApp({
   // const
   url:'https://cms.spiriert.de/',
+  routes: [
+    'texts'
+  ],
+
   // data
-  data: {
-    descriptions: []
-  },
+  data: {},
+
   // methods
-  load(route,target) {
+  load(route) {
+    this.data[route] = []
     fetch(this.url+route)
       .then(response => response.json())
-      .then(data => this.data[target] = data)
+      .then(data => this.data[route] = data)
   },
+
   mounted() {
-    this.load('texts','descriptions')
+    for (let r of this.routes) {
+      this.load(r)
+    }
   }
 }).mount()
