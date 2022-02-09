451a288d781a78c896230632e8a91e71414937af
theCVDH <c.cooly1111@web.de>
2022-02-09 15:21:50 +0100

    server

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,50 @@
-from flask import Flask
+from flask import Flask,redirect
 from redis import Redis
+import requests
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-@app.route('/')
-def hello():
-    redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+URL = "https://api.spiriert.de/"
+CODES = {}
+
+headers={"Authorization":"Bearer "}
+print(requests.get(URL+"items/qrcodes").json()["data"])
+for item in requests.get(URL+"items/qrcodes",headers=headers).json()["data"]:
+    CODES[item["Code"]] = item["id"]
+
+print(CODES)
+
+@app.route("/")
+def redirect_qr_():
+    return redirect("https://google.com")
+
+@app.route("/<code>")
+def redirect_qr(code):
+    if not code in CODES:
+        print(code +" not in CODES")
+        return redirect("https://google.com")
+    response = requests.post(URL+"items/qr_scan/",json={"code": CODES[code]},headers=headers)
+    print(response, URL+"items/qr_scan/")
+    return redirect("https://google.com")
+
+
+@app.route("/update")
+def update():
+    for item in requests.get(URL+"/items/qrcodes",headers=headers).json()["data"]:
+        CODES[item.code] = item.id
+    return ('', 204)
+
+
+@app.route("/users")
+def update():
+    for item in requests.get(URL+"/items/qrcodes",headers=headers).json()["data"]:
+        CODES[item.code] = item.id
+    return ('', 204)
+
+app.run(host="0.0.0.0",port=4321)
+
+
 
 
 if __name__ == "__main__":
